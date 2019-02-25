5599e06b259b66d16634fbfa01e3ae8f50a74e20
Andrey Gaisinskii <andygaisinskii@gmail.com>
2019-02-25 19:47:19 +0000

    almost done with init setup of the project

---
+++ src/layouts/MainLayout/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="rofl">test</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.rofl {
+  font-size: 30px;
+  color: $brand;
+}
+</style>
